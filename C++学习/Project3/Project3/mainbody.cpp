a038dad5fac78af8214f69b18b38cc65af4767bd
cmao2u <cmao2u@gmail.com>
2025-10-18 21:22:31 +0800

    更新

--- C++学习/Project3/Project3/mainbody.cpp
+++ C++学习/Project3/Project3/mainbody.cpp
@@ -186,6 +186,202 @@ bool issame(cube& c1, cube& c2)
 }
 */
 
+/*4.2对象特性
+构造函数和析构函数
+class Person
+{
+public:
+	Person()
+	{
+		cout << "这是构造函数" << endl;
+	}
+	~Person()
+	{
+		cout << " 这是析构函数" << endl;
+	}
+
+};
+void test01()
+{
+	Person p;
+}*/
+/*4.2.2拷贝构造函数与调用
+class person
+{
+public:
+	person()
+	{
+		cout << "这是构造函数（无参)" << endl;
+	}
+	person(int a)		
+	{
+		 age = a;
+		cout << "这是构造函数(有参)" << endl;
+	}
+	person(const person& p)
+	{
+		age = p.age;
+		cout << "这是拷贝构造函数" << endl;
+	}
+	~person()
+	{
+		cout << "这是析构函数" << endl;
+	}
+private: 
+	int age;
+};
+void test01()
+{
+	person p1;
+}
+void test02()
+{
+	person p2(10);
+}*/
+/*
+class person
+{
+public:
+
+	person()
+	{
+		cout << "这是一个无参构造函数" << endl;
+	}
+	person(int a)
+	{
+		age = a;
+		cout << "这是一个有参构造函数" << endl;
+	}
+	person(const person& p)
+	{
+		age = p.age;
+		cout << "这是一个拷贝构造函数" << endl;
+	}
+	~person()
+	{
+		cout << "这是一个析构函数" << endl;
+	}
+	
+public:
+	int age;
+};
+void test01()
+{
+	person p1(10);
+	person p2(p1);
+	cout << "p2的年龄是" << p2.age<<endl;
+
+}
+void test02(person p)
+{
+
+}
+void test03()
+{
+	person p1;
+	test02(p1);//运行拷贝构造函数person p(test02)=p1(test03)
+}
+person test04()
+{
+	person p2;
+	cout << (int*)&p2 << endl;
+	return person(p2);//去掉person触发NRVO
+}
+void test05()
+{
+	person p3 = test04();
+	cout << (int*)&p3 << endl;
+}
+*/
+
+/*4.2.5浅拷贝与深拷贝
+class person
+{
+public:
+	int *height;
+	person(int a)
+	{
+		height = new int(a);
+	}
+	person(const person& p)
+	{
+		height=new int(*p.height);
+	}
+	~person()
+	{
+		if (height != NULL)
+		{
+			delete height;
+			height = NULL;
+
+		}
+	}
+	
+	
+};
+void test01()
+{
+	person p1(173);
+	person p2(p1);
+	cout << "p1身高是" << *p1.height << endl;
+	cout << "p2身高是" << *p2.height << endl;
+}*/
+/*4.2.6初始化列表
+class person
+{
+public:
+	person(int a,int b,int c):m_a(a),m_b(b),m_c(c)
+	{ }
+	int m_a;
+	int m_b;
+	int m_c;
+
+};*/
+/*4.2.7
+子类
+class phone
+{
+public:
+	string p_name;
+	phone(string name)
+	{
+		p_name = name;
+		cout << "Phone构造" << endl;
+	}
+	~phone()
+	{
+		cout << "phone析构" << endl;
+	}
+};
+class person
+{
+public:
+	string name;
+	phone phone;
+	person(string name, string p_name) :name(name), phone(p_name)
+	{
+		cout << "Person构造" << endl;
+	}
+	~person()
+	{
+		cout << "Person析构" << endl;
+	}
+};
+void test01()
+{
+	person p("张三", "honor");
+	cout << p.name << "用" << p.phone.p_name << endl;
+};*/
+/*4.27
+static
+class person
+{
+public:
+	static int age;
+
+};
+int person::age = 18; 
+*/
 int main()
 {
 	/*func(1, 3.12);
@@ -250,7 +446,7 @@ int main()
 	}
 */
 
-
+	/*案例2
 	point p1;
 	p1.setX(10);
 	p1.setY(10);
@@ -260,7 +456,30 @@ int main()
 	center.setX(10);
 	center.setY(0);
 	c1.setcenter(center);
-	isInCircle(c1, p1);
+	isInCircle(c1, p1);*/
+
+	//test01();
+	//Person p;
+
+
+	/*person p1;
+	person p2(10);
+	person p3(p2);*/
+	/*4.2.2
+	拷贝构造函数与调用
+	test01();
+	test02();*/
+
+	/*4.2.3拷贝函数的调用时机
+	test01();
+	test03();
+	test05();*/
+
+	//test01();
+
+
+	//test01();
+	//cout << person::age << endl;
 
 
 	return 0;
--- 数据结构/数据结构/001.cpp
+++ 数据结构/数据结构/001.cpp
@@ -37,6 +37,26 @@ double function02(int n, double a[], double x)
 	}
 	return p;
 }*/
+clock_t start,finish;
+double duration;
+double function01(int n, double a[], double x)
+{
+	double p = 1/a[0];
+	for (int i = 1; i <= n; i++)
+	{
+		p += pow(x, i) / a[i];
+	}
+	return p;
+}
+double function02(int n, double a[], double x)
+{
+	double p = 1 / a[n];
+	for (int i = n; i > 0; i--)
+	{
+		p = (p * x + (1 / a[i - 1]));
+	}
+	return p;
+}
 int main()
 {/*
 	//printN(1000);
@@ -70,5 +90,30 @@ int main()
 	double duration02 = double(finish - start) / CLOCKS_PER_SEC;
 	cout << "function02 时间: " << duration02 << "s" << endl;
 	cout << function02(n, a, x) << endl;	*/
+
+	double a[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21 };// 多项式系数
+	int n = 20;
+	double x = 10.0;
+	const int repeat = 1000000;
+	clock_t start = clock();
+	for (int i = 0; i < repeat; i++)
+	{
+		function01(n,a,x);
+	}
+	clock_t finish = clock();
+	double duration01 = double(finish - start) / CLOCKS_PER_SEC;
+	cout << "function01 时间: " << duration01 << "s" << endl;
+	cout << function01(n, a, x) << endl;
+	 start = clock();
+	for (int i = 0; i < repeat; i++)
+	{
+		function02(n, a, x);
+	}
+	 finish = clock();
+	double duration02 = double(finish - start) / CLOCKS_PER_SEC;
+	cout << "function02 时间: " << duration02 << "s" << endl;
+	cout << function02(n, a, x) << endl;
+
+
 return 0;
 }
